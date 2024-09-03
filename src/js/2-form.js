890f30d7075fb0bd6af3306c78f7ff5c993a93f3
Maricnka_Lozynska <masyakuk@gmail.com>
2024-09-03 09:59:13 +0300

    update form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,24 +1,24 @@
 let formData = { email: "", message: "" };
 
 const feedbackForm = document.querySelector(".feedback-form");
-
 const localStorageKey = "feedback-form-state";
+
 function loadFormData() {
   try {
-    const formDataSave = localStorage.getItem('feedback-form-state');
+    const formDataSave = localStorage.getItem(localStorageKey);
     if (formDataSave) {
       formData = JSON.parse(formDataSave);
-      feedbackForm.elements.email.value = formData.email;
-      feedbackForm.elements.message.value = formData.message;
+      feedbackForm.elements.email.value = formData.email || "";
+      feedbackForm.elements.message.value = formData.message || "";
     }
   } catch (error) {
-    console.log('Error parsing saved data local storage:', error);
+    console.log("Error parsing saved data local storage:", error);
   }
 }
 loadFormData();
 
 feedbackForm.addEventListener("input", (event) => {
-  formData = { ...formData, [event.target.name]: event.target.value.trim() };
+  formData = { ...formData, [event.target.name]: event.target.value.trim() }
   
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
   
@@ -26,8 +26,8 @@ feedbackForm.addEventListener("input", (event) => {
 
 feedbackForm.addEventListener("submit", (event) => {
   event.preventDefault(); 
-   if (!formData.email.trim() || !formData.message.trim()) {
-    alert('Fill please all fields');
+    if (!formData.email.trim()|| !formData.message.trim()) {
+    alert("Fill please all fields");
     return;
 
     }
@@ -36,8 +36,8 @@ feedbackForm.addEventListener("submit", (event) => {
   }
   
   localStorage.removeItem(localStorageKey);
-
+  
   feedbackForm.reset();
 
-    formData = { email: "", message: "" };
+formData = { email: "", message: "" }; 
 });
